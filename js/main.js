ae977baadbad09e1a91d7d29967859b8985a3c03
Maxim <maxipy@ya.ru>
2021-04-27 20:27:43 +0400

    add offers section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
    
-    const progressImg = document.querySelector('.progress-img'); 
-    
-    
+    const progressImg = document.querySelector('.progress-img');
+    const offersControlBtn = document.querySelectorAll('.offers-control-btn');
+    const offersCardPriceValue = document.querySelectorAll('.offers-card__price-value');
+    const offersCardPeriod = document.querySelectorAll('.offers-card__period');
+    const yearPrice = [240, 360, 600];
+    const monthPrice = [20, 30, 50] 
+        
 
     window.onload = function(){
         let progressImgImg = document.querySelector('.progress-img img');
@@ -16,7 +20,38 @@
     });
 
 
+    function deactivate(){
+        offersControlBtn.forEach(item=>{
+            item.classList.remove('active');
+        })
+    };
 
+    offersControlBtn.forEach(item=>{
+        item.addEventListener('click', (event)=>{
+            if(!item.classList.contains('active')){
+                deactivate();
+                item.classList.add('active');
+                console.log(event.target.classList);
+                if(event.target.classList.contains('year-btn')){
+                    offersCardPriceValue.forEach((elem, i)=>{
+                        elem.textContent = yearPrice[i]
+                    });
+                    offersCardPeriod.forEach(elem=>{
+                        elem.textContent = 'Per of year';
+                    });
+                };
+                if(event.target.classList.contains('month-btn')){
+                    offersCardPriceValue.forEach((elem, i)=>{
+                        elem.textContent = monthPrice[i]
+                    });
+                    offersCardPeriod.forEach(elem=>{
+                        elem.textContent = 'Per of month';
+                    });
+                };
+            }
+           
+        });
+    });
 
 
 
